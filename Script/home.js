d5c1f8f4bb4901bab2876a12c66712ef13b51208
varun0948 <ravalvarun10@gmail.com>
2025-05-13 08:19:33 +0530

    six commit

--- Script/home.js
+++ Script/home.js
@@ -20,3 +20,21 @@ if (searchQuery) {
     }
   });
 }
+
+document.getElementById("search-btn").addEventListener("click", function () {
+  const searchVal = document.getElementById("search").value.toLowerCase();
+  const tripCards = document.querySelectorAll(".trip-card");
+
+  tripCards.forEach(function (card) {
+    const title = card.querySelector(".trip-title").textContent.toLowerCase();
+    const description = card
+      .querySelector(".trip-description")
+      .textContent.toLowerCase();
+
+    if (title.includes(searchVal) || description.includes(searchVal)) {
+      card.style.display = "block";
+    } else {
+       card.style.display="none"
+    }
+  });
+});
